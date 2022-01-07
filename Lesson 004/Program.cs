d885db0aa1543400cbfcbcd85e88d3f328694b8d
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-07 22:04:41 +0300

    Lesson 004

---
+++ Lesson 004/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_004
+{
+    class Program
+    {
+        //Convert sınıfının Toint32 metoduna "78" parametresini gönderin
+        //Bu ifadeyi bir değişkene atayın
+        //Değişkeni ekrana yazdırın
+        static void Main(string[] args)
+        {
+            string can = "78";
+            int cevrim = Convert.ToInt32(can);
+            Console.WriteLine(cevrim);
+            Console.ReadLine();
+        }
+    }
+}
