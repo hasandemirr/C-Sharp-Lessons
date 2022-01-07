536eb6859f4d148b2876cacae0ed68a7da5a8015
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-07 22:04:03 +0300

    Lesson 002

---
+++ Lesoon 002/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesoon_002
+{
+    class Program
+    {
+        //Kullanıcıya nasılsın diye sorun
+        //Kullanıcının cevabını iletin.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba ,"); //writeline bir satır aşağı atlar
+            Console.Write("Bugün Nasılsın? ");
+            var hal = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ben de "+hal);         
+            Console.ReadLine(); //Console'u açmak için readline kullanılır
+        }
+    }
+}
