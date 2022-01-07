9ba476a4bc8be5eff256872de7821642d986b5e0
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-07 22:40:59 +0300

    Lesson 007

---
+++ Lesson 007/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_007
+{
+    class Program
+    {
+        // Yaşı sorun
+        // 5 eksiği yaşında gösteriyorsunuz şeklinde yazdırın
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaç Yaşınızdasınız? ");
+            var yas = Console.ReadLine();
+            int yasim = Convert.ToInt32(yas);
+            yasim = yasim - 5;
+            Console.Write(yasim);
+            Console.Write(" yaşında gösteriyorsunuz.");
+            Console.ReadLine();
+        }
+    }
+}
