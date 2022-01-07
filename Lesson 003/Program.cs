81823cc95b4481ded0a8b3fc1377fd8340f1eda6
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-07 22:04:26 +0300

    Lesson 003

---
+++ Lesson 003/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_003
+{
+    class Program
+    {
+        //kullanıcıya yaşını sorun
+        //En güzel yaşın kullanıcının yaşı olduğunu belirtin
+        //bip sesi ile programı sonlandırın
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç yaşındasınız ? ");
+            var yas = Console.ReadLine();
+            Console.WriteLine("En güzel yaş " + yas);
+            Console.ReadLine();
+            Console.Beep(); //bip sesi alma komutu
+
+        }
+    }
+}
