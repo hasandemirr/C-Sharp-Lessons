177c6b95db23b4b12e5f8d7b711df2aec4844ca5
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-07 22:40:48 +0300

    Lesson 006

---
+++ Lesson 006/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_006
+{
+    class Program
+    {
+        // Console sınıfının Readline metodundan dönen değeri değişkene atayın
+        // Convert sınıfının ToInt32 metoduna bu değişkeni parametre olarak gönderin 
+        // Bu ifadeyi başka bir değişkene atayın
+        static void Main(string[] args)
+        {
+            var sss = Console.ReadLine();
+            int zzz = Convert.ToInt32(sss);
+            Console.WriteLine(sss);
+            Console.ReadLine();
+        }
+    }
+}
