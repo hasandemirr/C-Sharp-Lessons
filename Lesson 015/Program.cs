0c34f44b6946c34f27fcacc4d589361d0b7ee280
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-08 17:37:17 +0300

    Lesson 0015

---
+++ Lesson 015/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ornek1();
+            Console.ReadLine();
+        }
+
+        private static void ornek1()
+        {
+            if(true)
+            {
+                Console.WriteLine("Bu kod her zaman çalışır.");
+            }
+            if(DateTime.Today.Day==13)
+            {
+                Console.WriteLine("Ayın 13'ü geldi indirim zamanı.");
+            }
+            else
+            {
+                Console.WriteLine("İndirim için lütfen ayın 13'ü bekleyin.");
+            }
+        }
+    }
+}
