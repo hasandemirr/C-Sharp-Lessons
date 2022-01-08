27471ee7a13d96457b58d9bb7103d39d343317cf
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-08 17:37:04 +0300

    Lesson 0016

---
+++ Lesson 016/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_016
+{
+    class Program
+    {
+        // Kullanıcıdan yaşını isteyin
+        // Reşitse Ehliyet alıp alamayacağını geri bildirin
+        static void Main(string[] args)
+        {
+            resit();
+            Console.ReadLine();
+        }
+
+        private static void resit()
+        {
+            Console.WriteLine("kaç yaşınızdasınız?");
+            int yas = Convert.ToInt32(Console.ReadLine());
+            if(yas>=18)
+            {
+                Console.WriteLine("ehliyet alabilirsiniz.");
+            }
+            else
+            { 
+                Console.WriteLine("ehliyet alamazsınız"); 
+            }
+
+
+        }
+    }
+}
