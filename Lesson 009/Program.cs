2308e3775e1511cdf74f81cd7f77e7df1d47567e
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-08 17:39:57 +0300

    Lesson 009

---
+++ Lesson 009/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_009
+{
+    class Program
+    {
+        // Environment sınıfının TickCount elemanını bir değişkene atayın
+        // ms cinsinden olan bu değeri dakika olarak ekrana yazdırın
+        // not: küsüsratlı olabilsin
+        static void Main(string[] args)
+        {
+            var tms = Environment.TickCount;
+            var ts = tms / 1000;
+            var dk = ts / 60;
+            var s = dk / 60;
+            Console.WriteLine(s + " saat");
+            Console.ReadLine();
+        }
+    }
+}
