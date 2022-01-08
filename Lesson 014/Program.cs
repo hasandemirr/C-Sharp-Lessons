0414a612e8864bf0e5dcdfa1c7256cdf0a405da4
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-08 17:37:29 +0300

    Lesson 014

---
+++ Lesson 014/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_014
+{
+    class Program
+    {
+        /* bir bool değişkene şu ifadeyi atayalım
+         * Directory sınıfının Exists metoduna "test parametresini gönderelim" */
+        static void Main(string[] args)
+        {
+            bool varmı = Directory.Exists("test");
+            Console.ReadLine();
+        }
+    }
+}
