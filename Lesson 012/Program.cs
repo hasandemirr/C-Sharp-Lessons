7c14414db85602ec06575603a3e9a2274c77544a
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-08 17:39:18 +0300

    Lesson 012

---
+++ Lesson 012/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sayı = 5;
+            Console.WriteLine("Sayı: "+sayı);
+            Console.WriteLine("Sayının karesi: " + sayı * sayı);
+            Console.WriteLine("Sayının yarısı: " + sayı / 2);
+            Console.WriteLine("Sayının 2 ile bölümünden kalan: " + sayı % 2);
+            Console.ReadLine();
+        }
+    }
+}
