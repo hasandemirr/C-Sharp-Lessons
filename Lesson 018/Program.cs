7fb038e5551059276daca214292edb1ad1e0476a
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-09 18:05:21 +0300

    Lesson 018

---
+++ Lesson 018/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_018
+{
+    class Program
+    {
+        /* Kullanıcıdan kare ya da daire için parametreleri aldıktan sonra alanı hesaplayan kod */
+        static void Main(string[] args)
+        {
+            ornek();
+            Console.ReadLine();
+        }
+
+        private static void ornek()
+        {
+            Console.WriteLine("Kare mi, Daire mi?");
+            string cevap = Console.ReadLine();
+            if(cevap=="kare"||cevap=="Kare")
+            {
+                Console.WriteLine("Kenar uzunluğunu giriniz.");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Karenin alanı = " + a * a);
+            }
+            else if(cevap=="Daire"||cevap=="daire")
+            {
+                Console.WriteLine("Yarıçap uzunluğunu giriniz.");
+                int r = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dairenin alanı = " + Math.PI * Math.Pow(r , 2));
+                //Math.PI Pi'yi çağırır
+                //Math.Pow (r,2), r^2 işlemini yapar
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz Şekil!");
+            }
+
+        }
+    }
+}
