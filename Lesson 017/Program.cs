6d366ccd5ccf19a907f04a857e46fa81f243b8f7
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-09 18:05:12 +0300

    Lesson 017

---
+++ Lesson 017/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_017
+{
+    class Program
+    {
+        // Pc: Hangi programlama dilini seviyorsunuz?
+        // K:
+        // cevap c# ise harika, php ise güzelmiş, farklı bir cevap ise ama c# da güzel
+        static void Main(string[] args)
+        {
+            ornek();
+            Console.ReadLine();
+        }
+
+        private static void ornek()
+        {
+            Console.WriteLine("Hangi programlama dilini seviyorsunuz");
+            string cevap = Console.ReadLine();
+            if(cevap=="c#"|| cevap == "C#")
+            {
+                Console.WriteLine("Harika!");
+            }
+            else if(cevap=="php" || cevap == "PHP")
+            {
+                Console.WriteLine("Fevkalade!");
+            }
+            else
+            {
+                Console.WriteLine("C# da güzel.");
+            }
+        }
+    }
+}
