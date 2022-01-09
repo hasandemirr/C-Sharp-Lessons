46f6183c27b4fcf3a8e5fb53d750685fd2373f2e
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-09 18:06:18 +0300

    Lesson 022

---
+++ Lesson 022/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_022
+{
+    class Program
+    {
+        // 65 yaş üzerine ve öğrencilere ücretsiz geçiş hakkı veren kod
+        static void Main(string[] args)
+        {
+            Veya(); // veya koşulu || ile gösterilir, koşullardan birisinin karşılanması yeterdir
+            Console.ReadLine();
+        }
+
+        private static void Veya()
+        {
+            Console.WriteLine("Kaç yaşınızdasınız?");
+            int cevap1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Öğrenci misiniz? (e/h)");
+            string cevap2 = Console.ReadLine();
+            if(((cevap1>0 && cevap1<=24) && cevap2 == "e") || cevap1>=65)
+            {
+                Console.WriteLine("İndirimli geçiş");
+            }
+            else
+            {
+                Console.WriteLine("Ücretli geçiş");
+            }            
+        }
+    }
+}
