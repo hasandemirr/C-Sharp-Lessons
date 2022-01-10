ebb1532041736fe2d4615d2b12e79364696c9afd
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-10 18:01:24 +0300

    Lesson 024

---
+++ Lesson 024/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InLineif();
+            Console.ReadLine();
+        }
+
+        private static void InLineif()
+        {
+            // if else if ile aynı amaç için kullanılır
+            int sayi = 9;
+            bool _3unkatimi = 9 % 3 == 0 ? true : false;
+            string kullanici_sehir = Console.ReadLine();
+            string sehir = kullanici_sehir == "İstanbul" ? "34" : kullanici_sehir == "istanbul" ? "34" : "0";
+            // kullanici_sehir İstanbul'a eşitse 34, istanbul'a eşitse 34, değilse 0 yazdıran kod.
+            Console.WriteLine(sehir);
+
+        }
+    }
+}
