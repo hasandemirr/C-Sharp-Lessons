f0ee694309c2220c51f7e0a1016a3a96d77a3419
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-10 18:02:36 +0300

    Lesson 031

---
+++ Lesson 031/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_031
+{
+    class Program
+    {
+        // Kullanıcının doğduğu yılı sorgulayarak yaşını hesaplayan metod
+        static void Main(string[] args)
+        {          
+            int yasim = yasibul();
+            Console.WriteLine("Yaşınız " +yasim);
+            Console.ReadLine();
+        }
+
+        private static int yasibul()
+        {
+            return DateTime.Today.Year - yıl();
+        }
+
+        private static int yıl()
+        {
+            Console.WriteLine("Doğduğunuz yılı giriniz");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
