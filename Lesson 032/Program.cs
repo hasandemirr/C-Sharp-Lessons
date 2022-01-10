ebe00cda911295d611cd4aca5d650449cde611a0
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-10 18:02:46 +0300

    Lesson 032

---
+++ Lesson 032/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_032
+{
+    class Program
+    {
+        //Kullanıcının yaşının rakamları toplamını ekrana yazdıran bir metod yazalım.
+        static void Main(string[] args)
+        {           
+            rakamlar();
+            Console.ReadLine();
+        }
+
+        private static void rakamlar()
+        {
+            int yaslar = yasibul();
+            int a = yaslar / 10;
+            int b = yaslar % 10;
+            Console.WriteLine("Yaşınız " + yaslar);
+            Console.WriteLine("Basamak toplamı = " + (a + b));
+
+        }
+
+        private static int yasibul()
+        {
+            return DateTime.Today.Year - yıl();
+        }
+
+        private static int yıl()
+        {
+            Console.WriteLine("Lutfen doğdunuz yılı giriniz");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
