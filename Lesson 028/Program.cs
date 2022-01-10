4c5e8b49ccdcdecdeafd9da8b1ffaf91646b224a
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-10 18:02:12 +0300

    Lesson 028

---
+++ Lesson 028/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_028
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switchusing();
+            ornek();
+            Console.ReadLine();
+        }
+
+        private static void ornek()
+        {
+            // kullanıcının seçtiği dile göre selam veren kod
+            Console.WriteLine("Dil seçiniz (tr/de/en)");
+            string cevap = Console.ReadLine();
+
+            switch(cevap)
+            {
+                case "tr":
+                    Console.WriteLine("Merhaba");
+                    break;
+                case "en":
+                    Console.WriteLine("Hello");
+                    break;
+                case "de":
+                    Console.WriteLine("Hallo");
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz dil.");
+                    break;
+
+
+            }
+        }
+
+        private static void switchusing()
+        {
+            //çok eşitlik olduğunda switch kullanılır
+            ConsoleColor renk = Console.BackgroundColor;
+            switch(renk)
+            {
+                case ConsoleColor.Black:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case ConsoleColor.Yellow:
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                        
+            }
+        }
+    }
+}
