d0ad90b4ac0b95dbba48650dea7a88279c61a5d9
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-10 18:02:27 +0300

    Lesson 030

---
+++ Lesson 030/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_030
+{
+    class Program
+    {
+        // sayıların ortalamasını metod ile ekrana yazdıran kod
+        static void Main(string[] args)
+        {
+            int sayi1 = sayiiste();
+            int sayi2 = sayiiste();
+            Console.WriteLine((sayi1 + sayi2) / 2);
+            Console.ReadLine();
+        }
+
+        private static int sayiiste()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
