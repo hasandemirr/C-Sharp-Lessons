59ff269c72bc97898514afa5d9491ffce021a89d
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-10 18:01:55 +0300

    Lesson 027

---
+++ Lesson 027/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_027
+{
+    class Program
+    {
+        // girilen sayi tek ise karesini çift ise küpünü alan kod
+        static void Main(string[] args)
+        {
+            ornek();
+            Console.ReadLine();
+        }
+
+        private static void ornek()
+        {
+            Console.WriteLine("Sayı giriniz");
+            int sayı = Convert.ToInt32(Console.ReadLine());
+
+            string sonuc = sayı % 2 == 1 ? "sayının küpü = " + sayı * sayı * sayı : "sayının karesi = " + sayı * sayı;
+            Console.WriteLine(sonuc);
+        }
+    }
+}
