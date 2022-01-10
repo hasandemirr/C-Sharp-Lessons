3667d5091c791efc633d8307b63427b64024ac22
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-10 18:01:45 +0300

    Lesson 026

---
+++ Lesson 026/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_026
+{
+    class Program
+    {
+        // vize ve final notlarına göre dersten geçeni belirleyen kod
+        static void Main(string[] args)
+        {
+            Kabus();
+            Console.ReadLine();
+        }
+
+        private static void Kabus()
+        {
+            Console.WriteLine("Vize notunuz kaç?");
+            double vize = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Final notunuz kaç?");
+            double final = Convert.ToDouble(Console.ReadLine());
+            vize = vize * 0.4;
+            final = final * 0.6;
+            double ortalama = vize + final;
+
+            string sonuc = ortalama >= 50 ? "Geçtiniz" : ortalama < 50 ? "Kaldınız" : "";
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
