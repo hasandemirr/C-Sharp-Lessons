a15eedde3c0685bfacdfa0e7bf31dd8f634e42e5
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-12 20:38:34 +0300

    Lesson 037

---
+++ Lesson 037/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_037
+{
+    class Program
+    {
+        // 10 gün sonrasını gün ay yıl formatında yazdıran örnek
+        static void Main(string[] args)
+        {
+            ornek();
+            Console.ReadLine();
+        }
+
+        private static void ornek()
+        {
+            DateTime simdi = DateTime.Now;
+            DateTime ongunsonra = simdi.AddDays(10);
+            Console.WriteLine(ongunsonra.ToString("dd.MM.yyyy"));
+        }
+    }
+}
