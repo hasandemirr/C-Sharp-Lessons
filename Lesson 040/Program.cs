30deb056905a6d5def56b49094b5936b88aa1adf
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-12 20:38:57 +0300

    Lesson 040

---
+++ Lesson 040/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_040
+{
+    class Program
+    {
+        // Hata fırlatma örneği
+        static void Main(string[] args)
+        {
+            ornek();
+            Console.ReadLine();
+        }
+
+        private static void ornek()
+        {
+            Console.WriteLine("1-3 arasında bir sayı giriniz");
+            string sayi = Console.ReadLine();
+            switch(sayi)
+            {
+                case "1":
+                    throw new Exception("Beğenmedim");
+                    break;
+                case "2":
+                    throw new Exception("Başka sayı bulamadın mı");
+                    break;
+                case "3":
+                    throw new Exception("Bu ne biçim bir sayı");
+                    break;
+            }
+        }
+    }
+}
