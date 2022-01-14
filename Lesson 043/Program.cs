f98e0f5d54ba92ee6a2a3d9ffd0ef2c42728200e
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-14 22:32:30 +0300

    Lesson 043

---
+++ Lesson 043/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kisiler = { "Ali", "Can", "tuba", "Cansu" };
+            int[] can = { 1, 3, 5, 7, 9 };
+            char[] karakter = { 'a', 'b', 'c' };
+            Console.WriteLine(kisiler[2]);
+            Console.WriteLine(can[3]);
+            Console.WriteLine(karakter[0]);
+            Console.ReadLine();
+        }
+    }
+}
