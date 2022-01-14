7b14f30aae3f5d6e7d12381350331772807df8bb
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-14 22:33:48 +0300

    Lesson 050

---
+++ Lesson 050/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_050
+{
+    class Program
+    {
+        // Sifreyi bilene kadar soran kod
+        static void Main(string[] args)
+        {
+            ornek();
+            Console.ReadLine();
+        }
+
+        private static void ornek()
+        {
+            int a= 123;
+
+            do
+            {
+                Console.WriteLine("Lütfen sifreyi giriniz");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            while (a != 123);
+
+            Console.WriteLine("Şifre doğru");
+
+        }
+    }
+}
