80201dc41ce15a4cadb0ab01770271f37d34cc71
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-14 22:34:35 +0300

    Lesson 055

---
+++ Lesson 055/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_055
+{
+    // Alfabeyi ekrana bastıran kod
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            alfabe();
+            Console.ReadLine();
+        }
+
+        private static void alfabe()
+        {
+            for (int i ='A'; i <= 'Z'; i++)
+            {
+                Console.Write((char)i+" "); // char(i) = ascii koduna karşılşık gelen harfi verir
+            }
+        }
+    }
+}
