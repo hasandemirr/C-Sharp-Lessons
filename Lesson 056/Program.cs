adf2cebf74eaa36a5e8cf4088dc1081171a790fa
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-14 22:34:44 +0300

    Lesson 056

---
+++ Lesson 056/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_056
+{
+    class Program
+    {
+        // Ekrana üçgen bastıran kod
+        static void Main(string[] args)
+        {
+            ornek();
+            Console.ReadLine();
+        }
+
+        private static void ornek()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
