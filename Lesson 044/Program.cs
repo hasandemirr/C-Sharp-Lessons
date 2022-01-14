49171e4b1fd38a10d9416a1f52f9306e4515539f
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-14 22:32:52 +0300

    Lesson 044

---
+++ Lesson 044/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_044
+{
+    class Program
+    {
+        //diziyi parçalara ayır son elemanı yazdır
+        static void Main(string[] args)
+        {
+            Ornek();
+            Console.ReadLine();
+
+        }
+
+        private static void Ornek()
+        {
+            string tarih = "29.11.2018";
+            string[] parcalar = tarih.Split('.');
+            Console.WriteLine(parcalar[2]);
+            int index = parcalar.Length - 3; // indexi parcalar -3 e atadık
+            Console.WriteLine(parcalar[index]);
+
+        }
+    }
+}
