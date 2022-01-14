06d5d435a95a8129f9b9e2a7547f187b518ea2eb
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-14 22:34:16 +0300

    Lesson 053

---
+++ Lesson 053/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_053
+{
+    class Program
+    {
+        // İstenilen sayının faktöriyelini alın
+        static void Main(string[] args)
+        {
+            Ornek();
+            Console.ReadLine();
+        }
+
+        private static void Ornek()
+        {
+            Console.WriteLine("Bir sayı giriniz");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int faktroriyel = 1;
+            for (int b = 1;  b<= a; b++)
+            {
+                faktroriyel = b * faktroriyel;
+            }
+            Console.WriteLine(faktroriyel);
+
+        }
+    }
+}
