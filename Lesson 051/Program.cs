507b3275a85c1ca2d75976167f68acd40886355a
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-14 22:33:54 +0300

    Lesson 051

---
+++ Lesson 051/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreachdongusu() ;
+            Console.ReadLine();
+        }
+
+        private static void foreachdongusu()
+        {
+            // dizilerdeki elemanlar arasındaki döngü için foreach kullanılır
+            string[] ulkeler = { "Türkiye", "Amerika", "Almanya" };
+            foreach(var item in ulkeler)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
