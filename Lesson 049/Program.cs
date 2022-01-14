b3f32237479f4bf716a73e06ad66f87ca01e49c8
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-14 22:33:39 +0300

    Lesson 049

---
+++ Lesson 049/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while_for_farki();
+            Console.ReadLine();
+        }
+
+        private static void while_for_farki()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            int sayi = 0;
+            while (sayi < 5)
+            {
+                Console.WriteLine(sayi);
+                sayi++;
+            }
+        }
+    }
+}
