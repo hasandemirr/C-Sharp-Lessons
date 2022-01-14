fa359e9f822abe79e5505fe8bb3297f7bae0de17
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-14 22:33:29 +0300

    Lesson 048

---
+++ Lesson 048/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NullableNedir();
+            ForDongusu();
+            Console.ReadLine();
+        }
+
+        private static void ForDongusu()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void NullableNedir()
+        {
+            string a = null;
+            //string class null olabilir
+
+            // int ve double struct olduklarından null olamazlar
+
+            Nullable<int> sayi = null;
+            DateTime tarih = new DateTime(); // datetime ı boş bırakırsanız garip bir tarih verir
+            Console.WriteLine();
+        }
+    }
+}
