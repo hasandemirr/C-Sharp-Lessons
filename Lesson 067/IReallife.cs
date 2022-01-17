132fb8aba806da05cc480e9d7946c201a181d835
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-18 00:30:55 +0300

    Lesson 067

---
+++ Lesson 067/IReallife.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_067
+{
+    interface IReallife
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+    class SqlServerCustomerDal : IReallife
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Updated");
+        }
+    }
+    class OracleServerCustomerDal : IReallife
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Updated");
+        }
+    }
+    class MySqlServerCustomerDal : IReallife
+    {
+        public void Add()
+        {
+            Console.WriteLine("MySql Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("MySql Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("MySql Updated");
+        }
+    }
+    class CustomerManager
+    {
+        public void Add(IReallife hasan)
+        {
+            hasan.Add();
+        }
+    }
+}
---
+++ Lesson 067/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager musteriyoneticisi = new CustomerManager();
+            musteriyoneticisi.Add(new SqlServerCustomerDal());
+
+            IReallife[] yeni = new IReallife[3] // Veriyi tüm klasslara girebilmek için dizi oluştururuz.
+            {
+                new OracleServerCustomerDal(),
+                new MySqlServerCustomerDal(),
+                new SqlServerCustomerDal()
+            };
+
+            foreach (var adana in yeni)
+            {
+                adana.Add();
+                adana.Update();
+                adana.Delete();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
