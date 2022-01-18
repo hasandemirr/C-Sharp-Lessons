ba1ed815a7165e554ac77b47e1da208ad20059ee
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-19 00:45:29 +0300

    Lesson 070

---
+++ Lesson 070/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer hasan = new SqlServer();
+            hasan.Add();
+            MySql heddas = new MySql();
+            heddas.Add();
+            hasan.Delete();
+            Console.ReadLine();
+        }
+    }
+    class Database
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by default");
+        }
+    }
+    class SqlServer:Database
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Eklendi");
+        }
+        public override void Delete()
+        {
+            Console.WriteLine("Silindi");
+        }
+    }
+    class MySql:Database
+    {
+
+    }
+}
