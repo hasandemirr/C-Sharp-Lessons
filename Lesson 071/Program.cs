606182663412c7cd6e1acc09f0cf0167f4fc4b29
hasandemirr <hasan.demir.0396@gmail.com>
2022-01-19 00:45:36 +0300

    Lesson 071

---
+++ Lesson 071/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database Hasan = new Oracle();
+            Hasan.Add();
+            Hasan.Delete();
+            Database Heddas = new SqlServer();
+            Heddas.Add();
+            Heddas.Delete();
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Database // Abstract classlar new'lenmez
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        public abstract void Delete(); 
+        // Abstract class içerisinde abstract class kullnıldığında boş olmalı ve override zorunludur.
+
+    }
+    class SqlServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Sql");
+        }
+    }
+    class Oracle : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Oracle");
+        }
+    }
+}
